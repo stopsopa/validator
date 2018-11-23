50e433b93003613260e78af1b882e06d38551a49
Szymon Dzialowski <sd@phase-ii.com>
2018-11-23 23:23:55 +0000

    check badge

--- update-badge.js
+++ update-badge.js
@@ -14,6 +14,7 @@ process.stdout.write(`\n\n`);
 
 const README_md = path.resolve(__dirname, 'README.md');
 
+
 if ( ! fs.existsSync(README_md) ) {
 
     throw `File '${README_md}' doesn't exist`;
@@ -27,14 +28,29 @@ content = content.split("\n");
 
 let reg = /^\[!\[Build Status\]/;
 
+let done = false;
 for (let i = 0, l = content.length ; i < l ; i += 1 ) {
 
     if (reg.test(content[i])) {
 
-        content[i] = content[i].replace(/v\d+\.\d+\.\d+/g, 'v' + package.version);
+        const n = 'v' + package.version;
+
+        content[i] = content[i].replace(/v\d+\.\d+\.\d+/g, n);
+
+        if (content[i].indexOf(n) > -1) {
+
+            done = true;
+        }
 
         break;
     }
+}
+
+if ( ! done ) {
+
+    throw `Badge not found or invalid in file '${README_md}'`;
+
+    process.exit(2);
 }
 
 fs.writeFileSync(README_md, content.join("\n"));
