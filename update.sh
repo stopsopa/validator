ec28992d840dd653af1351a519e8ee21c23854a4
stopsopa <grytysek@gmail.com>
2019-02-05 09:38:22 +0000

    update

--- update.sh
+++ update.sh
@@ -128,6 +128,7 @@ if [ "$DIFF" != "" ] || [ "$1" = "force" ]; then
     mv dist/test.js dist/spvalidation.min.js
 
                             node update-badge.js
+                            npx markdown-toc -i README.md
                             git add README.md
 
                             git add dist
