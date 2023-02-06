bee515ef577390ce5974821bfdd9a3bf44e476c9
Szymon Dzialowski <stopsopa@users.noreply.github.com>
2023-02-06 16:50:09 +0000

    Update each.js

--- validator/utils/each.js
+++ validator/utils/each.js
@@ -6,7 +6,6 @@ const isArray = require('./isArray');
 const isObject = require('./isObject');
 
 module.exports = function each(obj, fn, context) {
-    var r;
     if (isArray(obj)) {
         for (var i = 0, l = obj.length ; i < l ; ++i) {
             if (fn.call(context, obj[i], i) === false) {
